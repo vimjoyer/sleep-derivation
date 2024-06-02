9740748f098b31e110548dc4d9e6caddea688b6f
vimjoyer <140887703+vimjoyer@users.noreply.github.com>
2024-06-02 18:46:55 +0200

    remove nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,9 @@
 {
   description = "sleep derivation flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-  };
+  inputs = {};
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-  }: let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = eachSystem (system: {
-      default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {};
-    });
+  outputs = s: {
+    packages."x86_64-linux".default = import ./default.nix "x86_64-linux";
   };
 }
